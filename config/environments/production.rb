962578320e4e8927d1befd5d08d5c8a0da52a4ce
teratti-6 <t.m.t.k.m.s.k.n.k@gmail.com>
2020-05-30 07:24:51 +0900

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = {  :host => 'http://175.41.227.60' }
+  config.action_mailer.default_url_options = {  :host => Rails.application.credentials.host_server[:ip] }
   #送信方法を指定（この他に:sendmail/:file/:testなどがあります)
   config.action_mailer.delivery_method = :smtp
   #送信方法として:smtpを指定した場合は、このconfigを使って送信詳細の設定を行います
