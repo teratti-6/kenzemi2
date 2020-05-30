c706aca47e314470acb6b610448ad4c8bbf0d0b3
teratti-6 <t.m.t.k.m.s.k.n.k@gmail.com>
2020-05-30 16:19:41 +0900

    fix

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::ConfirmationsController < Devise::ConfirmationsController
-  default from: 'notifications@example.com'
+  # default from: 'notifications@example.com'
  
   def welcome_email
     @user = params[:user]
